f39817275eae7bb9fc040835fa97e9b316d84cb5
Auston Youngblood <95832900+austonyb@users.noreply.github.com>
2023-02-27 13:43:36 -0700

    Initial Commit

---
+++ quizMania/quizManiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  quizManiaApp.swift
+//  quizMania
+//
+//  Created by Auston Youngblood on 2/27/23.
+//
+
+import SwiftUI
+
+@main
+struct quizManiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
